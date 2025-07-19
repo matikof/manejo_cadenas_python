857dd830769f66d5bc8ed1ad7d9675086b46b832
matikof <matikof2014@gmail.com>
2025-07-19 16:39:31 -0300

    Terminamos la leccion de concatenacion de cadenas

--- concatenacion_cadenas.py
+++ concatenacion_cadenas.py
@@ -1,1 +1,11 @@
 #Concatenacion cadenas
+cadena1 = "hola"
+cadena2 = "Mundo"
+#concatenacion = "hola" + "Mundo"
+concatenacion = cadena1 + " " + cadena2
+print(concatenacion)
+
+
+#Utilizando el método join
+contatenacion = "".join([cadena1, " ", cadena2])
+print(contatenacion)
