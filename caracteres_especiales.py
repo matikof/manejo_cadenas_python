e523cb1e8b89348ccd98eb9bb0f49335b55613a8
matikof <matikof2014@gmail.com>
2025-07-19 16:05:31 -0300

    Terminamos con la leccion de caracteres especiales

---
+++ caracteres_especiales.py
@@ -0,0 +1,10 @@
+ #Caracteres especiales
+ #Usamos un salto de línea antes de la palabra mundo
+print("Hola \nMundo")
+
+#tabulador
+print("\tPython es genial")
+#Comilla simple o doble
+print('Juan\' Perez')
+#Barrra invertida
+print("c\\mi_carpeta")
