192fa3089a9707fb411ad62573894717d9b160be
matikof <matikof2014@gmail.com>
2025-07-19 15:53:30 -0300

    Terminamos con el archivo inmutabilidad_cadenas

---
+++ inmutabilidad_cadenas.py
@@ -0,0 +1,7 @@
+#inmutabilidad en cadenas
+cadena1 = "hola mundo"
+#cadena1[0] = "h" # error no podemos modificar los caracteres
+cadena2 = cadena1 # En este caso estamos usando una referencia
+cadena = "Adios"
+print(cadena1)#No estamos modificando, si creando uno nuevo
+
