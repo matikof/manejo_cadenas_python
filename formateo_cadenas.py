6c3f3382edd08f4eb0a893815e95f1f2b20b0578
matikof <matikof2014@gmail.com>
2025-07-19 22:04:46 -0300

    Terminamos con la lección de formateo de cadenas

--- formateo_cadenas.py
+++ formateo_cadenas.py
@@ -5,3 +5,6 @@ edad = 30
 #f-string
 mensaje = f"Hola, me llamo {nombre} y tengo {edad} años."
 print(mensaje)
+#format
+mensaje2 = "Hola , me llamo {} y tengo {} años.".format(nombre,edad)
+print(mensaje2)
