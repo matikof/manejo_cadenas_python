43099c70b380f8d63abf2ae3058746eb12aa417a
matikof <matikof2014@gmail.com>
2025-07-20 00:49:43 -0300

    Agregamos varios métodos a tener en cuenta

--- subcadenas.py
+++ subcadenas.py
@@ -10,3 +10,16 @@ print(f"Subcadena de la cadena original: {subcadena_hola}")
 #Obtenemos la subcadena mundo
 subcadena_mundo = cadena[6:11]
 print(f"Subcadena Mundo: {subcadena_mundo}")
+
+
+indice_mundo = cadena.find("mundo")
+print(f"El indice donde se encontro la palabra mundo es: {indice_mundo}")
+
+#Obtener el indice de la subcadena hola
+indice_hola = cadena.find("Hola")
+print(f"El indice de la subcadena hola: {indice_hola}")
+
+#Reemplazar una subcadena
+cadena = "hola mundo"
+nueva_cadena = cadena.replace("mundo","a todos")
+print(nueva_cadena)
