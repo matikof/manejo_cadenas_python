ecf12cfe099cdf1cb0cfde3c4e3aca337a67f31d
matikof <matikof2014@gmail.com>
2025-07-19 23:07:37 -0300

    Agregamos y terminamos con la lección de manejo de subcadenas

---
+++ subcadenas.py
@@ -0,0 +1,12 @@
+#Sub cadenas en python
+cadena = "Hola, mundo!"
+
+
+# Obtenemos la subcadena de hola [inicio:fin (sin incluirlo)]
+subcadena_hola = cadena[0:4]#
+print(f"Subcadena de la cadena original: {subcadena_hola}")
+
+
+#Obtenemos la subcadena mundo
+subcadena_mundo = cadena[6:11]
+print(f"Subcadena Mundo: {subcadena_mundo}")
