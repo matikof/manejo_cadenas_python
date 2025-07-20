356e95906ad7a557a6d8e1ad419700a54108b377
matikof <matikof2014@gmail.com>
2025-07-19 22:15:18 -0300

    Agregamos lección métodos de cadenas

---
+++ metodos_cadenas.py
@@ -0,0 +1,16 @@
+# Métodos de cadenas
+
+cadena1 = "Hola Mundo"
+print(f"Cadena Original: {cadena1}")
+#Se genera una nueva cadena, no es que estemos modificando la primera
+#Metodo upper para poner todo en mayusculas
+mayusculas = cadena1.upper()
+print(f"Cadena en mayusculas: {mayusculas}")
+
+#Método lower
+print(f"Cadena en minusculas: {cadena1.lower()}")
+
+cadena2 = " Juan Perez "
+#.strip() para eliminar espacios tanto al inicio como en el final
+print(f"Cadena con espacios: {cadena2}")
+print(f"Cadena sin espacios: {cadena2.strip()}")
