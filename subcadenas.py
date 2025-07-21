d722965ab63cddd6340f81b2c785bd280adb9e2d
matikof <matikof2014@gmail.com>
2025-07-20 23:56:41 -0300

    Terminamos con el tema de métodos de cadenas

--- subcadenas.py
+++ subcadenas.py
@@ -1,3 +1,10 @@
+#IMPORTANTE
+#Una cadena es inmutable así que al usar la mayoría de estos
+#Métodos no estamos modificando la cadena original, si no
+#mas bien estamos creando una nueva.
+#La original no se toca, queda siempre igual!!!!!
+
+
 #Sub cadenas en python
 cadena = "Hola, mundo!"
 
@@ -23,3 +30,18 @@ print(f"El indice de la subcadena hola: {indice_hola}")
 cadena = "hola mundo"
 nueva_cadena = cadena.replace("mundo","a todos")
 print(nueva_cadena)
+
+# Sustituir hola por adios
+cadena_nueva_hola = cadena.replace("hola", "Adios")
+print(f"Nueva cadena reemplazada: {cadena_nueva_hola}")
+
+#Generador de Email
+nombre = " Ubaldo Acosta Soto "
+nombre_normalizado = nombre.strip().replace(" ",".").lower()
+print(nombre_normalizado)
+
+
+nombre_empresa = " Global Mentoring "
+extension_dominio = " .com.mx "
+dominio_email_normalizado = "@" + nombre_empresa.strip().lower().replace(" ", "") + extension_dominio.lower().strip()
+print(dominio_email_normalizado)
